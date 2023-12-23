11b8544e6e91619803b560c6f12a39ed8ee6757b
Roombie <120424080+Roombie@users.noreply.github.com>
2023-12-23 12:34:10 -0400

    Update LivesCounter.cs

--- Assets/Scripts/UI/LivesCounter.cs
+++ Assets/Scripts/UI/LivesCounter.cs
@@ -2,14 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
 using UnityEngine.UI;
 using TMPro;
 
 public class LivesCounter : MonoBehaviour
 {
-
     public bool oldNumber = false;
-
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +18,17 @@ public class LivesCounter : MonoBehaviour
         GetComponent<TextMeshProUGUI>().text = livesNum.ToString();
         if (oldNumber) {
             Invoke("PlaySound", 0.5f);
-            Invoke("GoBackToLevel", 2);
+
+            PlayableDirector director = GameObject.FindObjectOfType<PlayableDirector>();
+
+            if (director != null) {
+                double timelineDuration = director.duration;
+
+                // Use the timeline duration as the delay before loading the scene
+                Invoke("GoBackToLevel", (float)timelineDuration);
+            } else {
+                Invoke("GoBackToLevel", 2f);
+            }
         }
     }
 
@@ -29,12 +38,5 @@ public class LivesCounter : MonoBehaviour
 
     void PlaySound() {
         GetComponent<AudioSource>().Play();
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
