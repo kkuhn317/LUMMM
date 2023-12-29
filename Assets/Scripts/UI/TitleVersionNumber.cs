8525711d1a7b224af9094399c6bf6137c8841456
Kevin Kuhn <55213985+kkuhn317@users.noreply.github.com>
2023-12-28 19:52:29 -0500

    version change to 1.0.1

---
+++ Assets/Scripts/UI/TitleVersionNumber.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleVersionNumber : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<TMPro.TextMeshProUGUI>().text = "Version " + Application.version;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
