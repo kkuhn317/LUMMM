fe8843f3aa5c1c7620193c75bc50fff62bf47ee0
Kevin Kuhn <55213985+kkuhn317@users.noreply.github.com>
2024-03-28 23:35:43 -0400

    lava bubble, better recoloring shader, and more

---
+++ Assets/Scripts/Objects/Enemies/LavaBubble.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class LavaBubble : EnemyAI
+{
+    [Header("Lava Bubble")]
+    private float jumpVelocity; // Set this with the the velocity.y in the inspector
+    public float jumpInitialDelay = 0f;
+    public float jumpCooldown = 1f;
+    private bool isJumping = false;
+    private float initialPositionY;
+    private float gravityInternal;
+    private SpriteRenderer spriteRenderer;
+
+    private float startJumpTime = 0f;   // Used to help time the jumps with other lava bubbles
+
+    protected override void Start()
+    {
+        initialPositionY = transform.position.y;
+        jumpVelocity = velocity.y;
+        velocity = new Vector2(0, 0);
+        gravityInternal = gravity;
+        gravity = 0;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        base.Start();
+        enabled = true; // Enable the enemy AI because we want it to jump from offscreen
+        Invoke(nameof(Jump), jumpInitialDelay);
+    }
+
+    private void Jump()
+    {
+        if (isJumping || objectState == ObjectState.knockedAway)
+        {
+            return;
+        }
+        if (!isJumping)
+        {
+            //print("time between jumps: " + (Time.time - startJumpTime) + " seconds");
+            startJumpTime = Time.time;
+            isJumping = true;
+            velocity = new Vector2(velocity.x, jumpVelocity);
+            gravity = gravityInternal;
+        }
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (objectState == ObjectState.knockedAway)
+        {
+            return;
+        }
+        spriteRenderer.flipY = velocity.y < 0;
+        if (isJumping && velocity.y <= 0 && transform.position.y <= initialPositionY)
+        {
+            isJumping = false;
+            transform.position = new Vector3(transform.position.x, initialPositionY, transform.position.z);
+            velocity = new Vector2(0, 0);
+            gravity = 0;
+            //print("time it took to jump: " + (Time.time - startJumpTime) + " seconds");
+            Invoke(nameof(Jump), jumpCooldown);
+        }
+    }
+}
