0123961dce693b5fed60e116d375270b2e8d2873
Roombie <120424080+Roombie@users.noreply.github.com>
2024-03-29 13:22:38 -0300

    Select level menu with arrow sprites and swipe detection

---
+++ Assets/Scripts/UI/SwipeController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SwipeController : MonoBehaviour, IEndDragHandler
+{
+    [SerializeField] int maxPage;
+    int currentPage;
+    Vector3 targetPos;
+    [SerializeField] Vector3 pageStep;
+    [SerializeField] RectTransform levelPagesRect;
+
+    [SerializeField] float tweenTime;
+    [SerializeField] LeanTweenType tweenType;
+    float dragThereshould;
+
+    private void Awake()
+    {
+        currentPage = 1;
+        targetPos = levelPagesRect.localPosition;
+        dragThereshould = Screen.width / 15;
+    }
+
+    public void Next()
+    {
+        if (currentPage < maxPage)
+        {
+            currentPage++;
+            targetPos += pageStep;
+            MovePage();
+        }
+    }
+
+    public void Previous()
+    {
+        if (currentPage > 1)
+        {
+            currentPage--;
+            targetPos -= pageStep;
+            MovePage();
+        }
+    }
+
+    void MovePage()
+    {
+        levelPagesRect.LeanMoveLocal(targetPos, tweenTime).setEase(tweenType);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (Mathf.Abs(eventData.position.x - eventData.pressPosition.x) > dragThereshould){
+            if (eventData.position.x > eventData.pressPosition.x) Previous();
+            else Next();
+        } else {
+            MovePage();
+        }
+    }
+}
