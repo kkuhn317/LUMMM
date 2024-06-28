6c854aa45bf503b10ff3d24e5f2fbb3afb12c761
Roombie <120424080+Roombie@users.noreply.github.com>
2024-06-28 00:35:57 -0400

    fixing some inaccuracies on The World of Spikes
    - SpikeBill's position
    - Pipe spawning grrol is faster
    - Adding line sprite on the sawblade trap

--- Assets/Scripts/Objects/Enemies/Grrol.cs
+++ Assets/Scripts/Objects/Enemies/Grrol.cs
@@ -5,9 +5,9 @@ public class Grrol : EnemyAI
     [Header("Grrol")]
     [SerializeField] private Transform rotationTarget; // The object to which rotation is applied
     [SerializeField] private float rotationSpeedMultiplier = 1f; // Multiplier for rotation speed
-    [SerializeField] private bool enableCameraShake = true; // whether to enable camera shake on touch wall
-    [SerializeField] private bool startMovementWhenTouchGround = true;
     [SerializeField] private float groundMovementSpeed = 5f;
+    [SerializeField] private bool startMovementWhenTouchGround = true;
+    [SerializeField] private bool enableCameraShake = true; // whether to enable camera shake on touch wall
 
     private Animator animator;
     private AudioSource audioSource;
@@ -27,12 +27,9 @@ public class Grrol : EnemyAI
 
         // THIS IS IF YOU WANT YOUR OBJECT ROTATES FASTER WHEN FALLING
         // rotation speed: velocity / radius
-        // Calculate linear velocity magnitude
-        //float linearVelocityMagnitude = Mathf.Sqrt(velocity.x * velocity.x + velocity.y * velocity.y);
-        // Calculate rotation speed based on linear velocity magnitude and multiplier
-        // float rotationSpeed = linearVelocityMagnitude * rotationSpeedMultiplier;
-        // Determine rotation direction based on whether the object is moving left
-        // float rotationDirection = movingLeft ? 1f : -1f;
+        // float linearVelocityMagnitude = Mathf.Sqrt(velocity.x * velocity.x + velocity.y * velocity.y);       // Calculate linear velocity magnitude
+        // float rotationSpeed = linearVelocityMagnitude * rotationSpeedMultiplier;                             // Calculate rotation speed based on linear velocity magnitude and multiplier
+        // float rotationDirection = movingLeft ? 1f : -1f;                                                     // Determine rotation direction based on whether the object is moving left
 
         // Calculate rotation speed based on the x component of velocity
         float rotationSpeed = Mathf.Abs(velocity.x) * rotationSpeedMultiplier;
@@ -42,9 +39,13 @@ public class Grrol : EnemyAI
 
         // Rotate the rotationTarget object around the z-axis
         if (rotationTarget != null)
+        {
             rotationTarget.Rotate(0, 0, rotationDirection * rotationSpeed * Time.deltaTime);
-        else
+        } 
+        else 
+        {
             Debug.Log("Rotation target missing. Please add which object you want to rotate");
+        }
 
         // Check if the object is moving
         bool isMoving = Mathf.Abs(velocity.x) > 0;
@@ -78,20 +79,21 @@ public class Grrol : EnemyAI
     {
         base.onTouchWall(other);
 
-        if (other == null)
+        if (other == null) 
         {
             Debug.LogError("Collision detected, but 'other' collider is null.");
             return;
         }
 
-        if (enableCameraShake) {
+        if (enableCameraShake) 
+        {
             // Get the CameraFollow component from the camera
             CameraFollow cameraFollow = Camera.main.GetComponent<CameraFollow>();
             // Trigger camera shake
             cameraFollow.ShakeCameraRepeatedly(0.25f, 2.0f, 1.0f, new Vector3(0f, 1f, 0f), 3, 0.1f);
         }
 
-        if (audioSource != null)
+        if (audioSource != null) 
         {
             audioSource.Play();
         }
