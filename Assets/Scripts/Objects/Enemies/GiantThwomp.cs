3f56eb84246f21383401907af5fd08d54e643784
Roombie <120424080+Roombie@users.noreply.github.com>
2024-07-08 14:11:52 -0400

    Adding victory dance animation

---
+++ Assets/Scripts/Objects/Enemies/GiantThwomp.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiantThwomp : EnemyAI
+{
+    [Header("Giant Thwomp")]
+    public float fallDetection = 0.2f;
+}
