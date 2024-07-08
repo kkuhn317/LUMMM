3fa5eccbc22f4c7eacfcc4e274580869a672102f
Roombie <120424080+Roombie@users.noreply.github.com>
2024-07-08 15:53:44 -0400

    Giant Thwomp script (not completed)

--- Assets/Scripts/Objects/Enemies/GiantThwomp.cs
+++ Assets/Scripts/Objects/Enemies/GiantThwomp.cs
@@ -5,5 +5,29 @@ using UnityEngine;
 public class GiantThwomp : EnemyAI
 {
     [Header("Giant Thwomp")]
-    public float fallDetection = 0.2f;
+    public SpriteRenderer front;
+    public Sprite idleSprite;
+    public Sprite angrySprite;
+
+    public enum ThwompStates {
+        Idle, // The Thwomp remains stationary, waiting for the player to come into range
+        Falling, // The Thwomp falls rapidly towards the player or a specific target area
+        Stunned, // After hitting the ground, the Thwomp might remain in a stunned state for a short period, unable to move
+        Rising, // The Thwomp rises back to its original position after completing its fall and stun duration
+        DetectPlayerLeft, // The Thwomp moves left after being stunned or rising if detects a player
+        DetectPlayerRight, // The Thwomp moves right after being stunned or rising if detects a player
+        DetectPlayerAbove, // The Thwomp detects the player above and rises (player has different death when spinning)
+        Vulnerable, // The Thwomp becomes vulnerable after being hit by Mario's cape, remaining in this state for a certain period
+    }
+    private ThwompStates currentState;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    private void ChangeState(ThwompStates newState)
+    {
+        currentState = newState;
+    }
 }
