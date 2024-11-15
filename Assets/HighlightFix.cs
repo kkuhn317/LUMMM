b44b2a2c05d77f046ca6e25d24227a1069b1c189
Roombie <120424080+Roombie@users.noreply.github.com>
2024-11-15 15:59:12 -0400

    pushed coin bug fix
    The End Coin Door used to open when the pushed coin was near the door, regardless of whether the player was near the pushed coin or not. The player could trigger the door opening by pressing up while the coin was in range

---
+++ Assets/HighlightFix.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Selectable))]
+public class HighlightFix : MonoBehaviour, IPointerEnterHandler, IDeselectHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!EventSystem.current.alreadySelecting)
+            EventSystem.current.SetSelectedGameObject(this.gameObject);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        this.GetComponent<Selectable>().OnPointerExit(null);
+    }
+}
--- Assets/Scripts/Level Specific/Coin Doors Maze/EndCoinDoor.cs
+++ Assets/Scripts/Level Specific/Coin Doors Maze/EndCoinDoor.cs
@@ -8,15 +8,17 @@ public class EndCoinDoor : CoinDoor
 {
     private bool coinTouchedDoor = false;
 
-    public GameObject pushCoin;     // The coin that can be used to open the door
+    public GameObject pushCoin; // The coin that can be used to open the door
+    public float maxPlayerCoinDistance = 0.85f; // Maximum distance between player and coin to open the door
 
     protected override void Start()
     {
         base.Start();
     }
 
-    protected override bool CheckForKey() {
-        // if the push coin is within 1 unit of the door, the door can be opened
+    protected override bool CheckForKey()
+    {
+        // If the push coin is within the door range, the door can be opened
         return coinTouchedDoor;
     }
 
@@ -27,7 +29,21 @@ public class EndCoinDoor : CoinDoor
 
     protected override bool PlayerAtDoor(MarioMovement playerScript)
     {
-        return CheckForKey() || (playerInRange && (!mustBeStanding || playerScript.onGround));
+        // Check if the coin is near the door
+        if (coinTouchedDoor && pushCoin != null)
+        {
+            // Ensure the player is near the coin as well
+            float distanceToCoin = Vector2.Distance(player.transform.position, pushCoin.transform.position);
+            Debug.Log($"Player to coin distance: {distanceToCoin}");
+
+            if (distanceToCoin <= maxPlayerCoinDistance)
+            {
+                return true;
+            }
+        }
+
+        // Fallback to normal player proximity check
+        return playerInRange && (!mustBeStanding || playerScript.onGround);
     }
 
     protected override void Close()
@@ -38,8 +54,10 @@ public class EndCoinDoor : CoinDoor
         }
     }
 
-    protected override void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject == pushCoin) {
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == pushCoin)
+        {
             coinTouchedDoor = true;
         }
         base.OnTriggerEnter2D(other);
