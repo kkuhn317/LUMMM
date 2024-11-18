9745c63c6c4e9fcf9cc0dfd9e89dc2c131727a66
Roombie <120424080+Roombie@users.noreply.github.com>
2024-11-18 13:52:27 -0400

    Hover effect on the game modifiers to see what they are

---
+++ Assets/HoverEffect.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject hoverPanel;
+    private bool isHovering = false;
+
+    // Called when the pointer enters the UI element or object
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isHovering = true;
+        StartHoverEffect();
+    }
+
+    // Called when the pointer exits the UI element or object
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHovering = false;
+        StopHoverEffect();
+    }
+
+    private void Update()
+    {
+        // Handle touch input for mobile
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector2 touchPosition = touch.position;
+                if (RectTransformUtility.RectangleContainsScreenPoint(
+                    GetComponent<RectTransform>(), touchPosition, Camera.main))
+                {
+                    isHovering = true;
+                    StartHoverEffect();
+                }
+            }
+            else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+            {
+                isHovering = false;
+                StopHoverEffect();
+            }
+        }
+    }
+
+    private void StartHoverEffect()
+    {
+        // Implement what happens when the hover starts
+        Debug.Log("Hover started");
+        hoverPanel.SetActive(true);
+    }
+
+    private void StopHoverEffect()
+    {
+        // Implement what happens when the hover ends
+        Debug.Log("Hover ended");
+        hoverPanel.SetActive(false);
+    }
+}
