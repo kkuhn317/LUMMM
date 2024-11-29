1c2e0a5cef082220dadd050e6e74650d68fe1a84
Roombie <120424080+Roombie@users.noreply.github.com>
2024-11-29 00:23:27 -0400

    dropdown list keyboard navigation

---
+++ Assets/Scripts/UI/UIDropdownScroller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIDropdownScroller : MonoBehaviour, ISelectHandler
+{
+    private ScrollRect scrollRect;
+    private float scrollPosition = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollRect = GetComponentInParent<ScrollRect>(true);
+
+        int childCount = scrollRect.content.transform.childCount - 1;
+        int childIndex = transform.GetSiblingIndex();
+
+        childIndex = Mathf.Clamp(childIndex, 0, childCount - 1);
+        
+        scrollPosition = 1 - ((float)childIndex / childCount);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        if(scrollRect)
+            scrollRect.verticalScrollbar.value = scrollPosition;
+    }
+}
