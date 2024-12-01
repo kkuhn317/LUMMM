86229681d4893fdf7cca62f8817c80877612724e
Roombie <120424080+Roombie@users.noreply.github.com>
2024-12-01 18:10:39 -0400

    scrollRect auto-scroll when using keyboard navigation fix It and goes to the current selected when you press the dropdown

--- Assets/Scripts/UI/UIDropdownScroller.cs
+++ Assets/Scripts/UI/UIDropdownScroller.cs
@@ -1,55 +1,92 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class UIDropdownScroller : MonoBehaviour, ISelectHandler, IPointerEnterHandler
+// Solution implemented by mandarinx
+// https://gist.github.com/mandarinx/eae10c9e8d1a5534b7b19b74aeb2a665
+[RequireComponent(typeof(ScrollRect))]
+public class UIDropdownScroller : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private ScrollRect scrollRect;
-    private float scrollPosition = 1;
-    private bool isPointerSelection = false; // Flag to track pointer-based selection
+    public float scrollSpeed = 10f;
+    private bool mouseOver = false;
 
+    private List<Selectable> m_Selectables = new List<Selectable>();
+    private ScrollRect m_ScrollRect;
+
+    private Vector2 m_NextScrollPosition = Vector2.up;
+    void OnEnable()
+    {
+        if (m_ScrollRect)
+        {
+            m_ScrollRect.content.GetComponentsInChildren(m_Selectables);
+        }
+    }
+    void Awake()
+    {
+        m_ScrollRect = GetComponent<ScrollRect>();
+    }
     void Start()
     {
-        scrollRect = GetComponentInParent<ScrollRect>(true);
-
-        if (scrollRect == null)
+        if (m_ScrollRect)
         {
-            Debug.LogWarning("ScrollRect component not found in parent hierarchy.");
-            return;
+            m_ScrollRect.content.GetComponentsInChildren(m_Selectables);
         }
-        if (scrollRect.content == null)
-        {
-            Debug.LogWarning("ScrollRect content is not assigned.");
-            return;
-        }
-
-        int childCount = scrollRect.content.transform.childCount - 1;
-        int childIndex = transform.GetSiblingIndex();
-
-        childIndex = Mathf.Clamp(childIndex, 0, childCount - 1);
-        scrollPosition = 1 - ((float)childIndex / childCount);
+        ScrollToSelected(true);
     }
+    void Update()
+    {
+        // Scroll via input.
+        InputScroll();
+        if (!mouseOver)
+        {
+            // Lerp scrolling code.
+            m_ScrollRect.normalizedPosition = Vector2.Lerp(m_ScrollRect.normalizedPosition, m_NextScrollPosition, scrollSpeed * Time.deltaTime);
+        }
+        else
+        {
+            m_NextScrollPosition = m_ScrollRect.normalizedPosition;
+        }
+    }
+    void InputScroll()
+    {
+        if (m_Selectables.Count > 0)
+        {
+            if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical") || Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+            {
+                ScrollToSelected(false);
+            }
+        }
+    }
+    void ScrollToSelected(bool quickScroll)
+    {
+        int selectedIndex = -1;
+        Selectable selectedElement = EventSystem.current.currentSelectedGameObject ? EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>() : null;
 
+        if (selectedElement)
+        {
+            selectedIndex = m_Selectables.IndexOf(selectedElement);
+        }
+        if (selectedIndex > -1)
+        {
+            if (quickScroll)
+            {
+                m_ScrollRect.normalizedPosition = new Vector2(0, 1 - (selectedIndex / ((float)m_Selectables.Count - 1)));
+                m_NextScrollPosition = m_ScrollRect.normalizedPosition;
+            }
+            else
+            {
+                m_NextScrollPosition = new Vector2(0, 1 - (selectedIndex / ((float)m_Selectables.Count - 1)));
+            }
+        }
+    }
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // Set the flag to indicate pointer interaction
-        isPointerSelection = true;
+        mouseOver = true;
     }
-
-    public void OnSelect(BaseEventData eventData)
+    public void OnPointerExit(PointerEventData eventData)
     {
-        // If the selection was caused by pointer interaction, reset the flag and skip
-        if (isPointerSelection)
-        {
-            isPointerSelection = false; // Reset the flag
-            return;
-        }
-
-        // Perform scrolling only for navigation input (keyboard/controller)
-        if (scrollRect)
-        {
-            Canvas.ForceUpdateCanvases(); // Ensure layout updates
-            scrollRect.verticalNormalizedPosition = scrollPosition;
-        }
+        mouseOver = false;
+        ScrollToSelected(false);
     }
 }
