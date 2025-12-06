85d29624920ed9a2cf21fc344670fbc45a1cea56
Roombie <roombieishere@gmail.com>
2025-12-06 03:55:05 -0400

    add retry and quit buttons on game over screen instead of taking you back to menu inmediately

--- Assets/Scripts/UI/GameOverScript.cs
+++ Assets/Scripts/UI/GameOverScript.cs
@@ -1,21 +1,96 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections;
 
 public class GameOverScript : MonoBehaviour
 {
     public float transitionChangeDelay = 6f;
+    public UnityEvent onButtonsReady;
 
-    void Start()
+    [SerializeField] private CanvasGroup gameOverCanvasGroup;
+    [SerializeField] private GameObject defaultSelectedOnReady;
+
+    private bool canPressButtons = false;
+    private bool optionChosen = false;
+
+    private void OnEnable()
     {
-        Invoke(nameof(ChangeScene), transitionChangeDelay);
+        canPressButtons = false;
+        optionChosen = false;
+
+        if (gameOverCanvasGroup != null)
+        {
+            gameOverCanvasGroup.interactable = false;
+            gameOverCanvasGroup.blocksRaycasts = false;
+        }
+
+        if (transitionChangeDelay > 0f)
+            Invoke(nameof(TriggerButtonsReady), transitionChangeDelay);
+        else
+            TriggerButtonsReady();
     }
 
-    void ChangeScene()
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    private void TriggerButtonsReady()
     {
         CursorHelper.ShowCursor();
-        FadeInOutScene.Instance.LoadSceneWithFade("SelectLevel");
+
+        canPressButtons = true;
+
+        if (gameOverCanvasGroup != null)
+        {
+            gameOverCanvasGroup.interactable = true;
+            gameOverCanvasGroup.blocksRaycasts = true;
+        }
+
+        onButtonsReady?.Invoke();
+        
+        if (defaultSelectedOnReady != null && EventSystem.current != null)
+        {
+            EventSystem.current.SetSelectedGameObject(defaultSelectedOnReady);
+        }
     }
 
+    private IEnumerator LockUIAfterFrame()
+    {
+        yield return null;
+
+        if (EventSystem.current != null)
+            EventSystem.current.SetSelectedGameObject(null);
+        
+        if (gameOverCanvasGroup != null)
+        {
+            gameOverCanvasGroup.blocksRaycasts = false;
+        }
+    }
+
+    public void OnRetryButtonPressed()
+    {
+        if (!canPressButtons || optionChosen) return;
+        optionChosen = true;
+        canPressButtons = false;
+
+        StartCoroutine(LockUIAfterFrame());
+
+        if (GameManager.Instance != null)
+            GameManager.Instance.RestartLevelFromBeginningWithFadeOut();
+    }
+
+    public void OnQuitButtonPressed()
+    {
+        if (!canPressButtons || optionChosen) return;
+        optionChosen = true;
+        canPressButtons = false;
+
+        StartCoroutine(LockUIAfterFrame());
+
+        if (FadeInOutScene.Instance != null)
+            FadeInOutScene.Instance.LoadSceneWithFade("SelectLevel");
+    }
 }
