61237878e6f439fad72c88134d7ec9a5a655a22c
Kevin Kuhn <55213985+kkuhn317@users.noreply.github.com>
2026-01-09 18:10:25 -0500

    fix slopes, prevent coin from being pushed against wall near it

--- Assets/Scripts/Gameplay/Player/Pushable.cs
+++ Assets/Scripts/Gameplay/Player/Pushable.cs
@@ -7,6 +7,8 @@ public class Pushable : MonoBehaviour
     ObjectPhysics physics;
     public float pushSpeed = 10;
     private MarioMovement playerScript;
+    public float stopWallDistance = 0f; // How far from the wall to stop pushing (to allow you to push it away from the wall)
+    public LayerMask wallLayer; // LayerMask for walls
 
     // Start is called before the first frame update
     void Start()
@@ -28,14 +30,16 @@ public class Pushable : MonoBehaviour
         GameObject mario = playerScript.gameObject;
         Rigidbody2D marioRb = mario.GetComponent<Rigidbody2D>();
 
-        if (mario.transform.position.x < transform.position.x && playerScript.moveInput.x > 0 && marioRb.velocity.x >= 0)
+        bool hitWall = WallDistanceCheck(mario.transform.position.x > transform.position.x);
+
+        if (mario.transform.position.x < transform.position.x && playerScript.moveInput.x > 0 && marioRb.velocity.x >= 0 && !hitWall)
         {
             physics.movingLeft = false;
             physics.velocity.x = pushSpeed;
             playerScript.FlipTo(true);
             playerScript.StartPushing(physics, pushSpeed);
         }
-        else if (mario.transform.position.x > transform.position.x && playerScript.moveInput.x < 0 && marioRb.velocity.x <= 0)
+        else if (mario.transform.position.x > transform.position.x && playerScript.moveInput.x < 0 && marioRb.velocity.x <= 0 && !hitWall)
         {
             physics.movingLeft = true;
             physics.velocity.x = pushSpeed;
@@ -45,6 +49,21 @@ public class Pushable : MonoBehaviour
             physics.velocity.x = 0;
             playerScript.StopPushing();
         }
+    }
+
+    // True = left, False = right
+    public bool WallDistanceCheck(bool direction)
+    {
+        if (stopWallDistance <= 0)
+        {
+            return false;
+        }
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction ? Vector2.left : Vector2.right, stopWallDistance, wallLayer);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        return false;
     }
 
     public void StopPushing()
